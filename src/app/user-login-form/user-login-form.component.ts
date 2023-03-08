c4750fce7f5d107e40f8a704d28050660ec3ef8a
Nikola Stefanovski <nikoding32@gmail.com>
2023-03-08 17:41:23 +0100

    Added movie card

--- src/app/user-login-form/user-login-form.component.ts
+++ src/app/user-login-form/user-login-form.component.ts
@@ -9,6 +9,8 @@ import { UserRegistrationService } from '../fetch-api-data.service';
 // This import is used to display notifications back to the user
 import { MatSnackBar } from '@angular/material/snack-bar';
 
+import { Router } from '@angular/router';
+
 @Component({
   selector: 'app-user-login-form',
   templateUrl: './user-login-form.component.html',
@@ -20,19 +22,24 @@ export class UserLoginFormComponent implements OnInit {
   constructor(
     public fetchApiData: UserRegistrationService,
     public dialogRef: MatDialogRef<UserLoginFormComponent>,
-    public snackBar: MatSnackBar) { }
+    public snackBar: MatSnackBar,
+    private router: Router
+    ) { }
 
   ngOnInit(): void {
   }
   // This is the function responsible for sending the form inputs to the backend
   loginUser(): void {
       this.fetchApiData.userLogin(this.userData).subscribe((result) => {
+        localStorage.setItem('user', result.user.Username);
+        localStorage.setItem('token', result.token);
     // Logic for a successful user login goes here! (To be implemented)
       this.dialogRef.close(); // This will close the modal on success!
       console.log(result)
       this.snackBar.open(result, 'OK', {
           duration: 2000
       });
+      this.router.navigate(['movies']);
       }, (result) => {
         console.log(result)
         this.snackBar.open(result, 'OK', {
